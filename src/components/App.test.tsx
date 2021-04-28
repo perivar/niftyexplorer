009b6e54e28a5465582e306b9b9d007e9a46587b
Per Ivar Nerseth <perivar@nerseth.com>
2021-04-28 20:37:26 +0200

    fixed test and the icon file links

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -5,8 +5,8 @@ import App from './App';
 process.env.REACT_APP_ELECTRUMX_MAINNET = 'dummy';
 process.env.REACT_APP_ELECTRUMX_TESTNET = 'dummy';
 
-test('renders search text', () => {
+test('renders Portfolio link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Search/i);
+  const linkElement = screen.getByText(/Portfolio/i);
   expect(linkElement).toBeInTheDocument();
 });
