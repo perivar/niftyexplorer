f0361f60cfe2e5c9c75e15b462ab0240c18502e8
Per Ivar Nerseth <perivar@nerseth.com>
2021-05-02 15:09:54 +0200

    improved explorer

--- src/components/Explorer/Explorer.tsx
+++ src/components/Explorer/Explorer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import * as bitcoin from 'bitcoinjs-lib';
+// import * as bitcoin from 'bitcoinjs-lib';
 // import * as slpParser from 'slp-parser';
 // import CryptoNFT from '../../crypto/slp/nft';
 import CryptoUtil from '../../crypto/util';
@@ -40,6 +40,8 @@ export const Explorer = ({ match }: { match: any }) => {
   const [query, setQuery] = useState<string>('');
   const [result, setResult] = useState<string>('');
 
+  const [totalInputValue, setTotalInputValue] = useState<number>(0);
+  const [totalOutputValue, setTotalOutputValue] = useState<number>(0);
   const [senders, setSenders] = useState<ISenders>({
     addresses: []
   });
@@ -58,40 +60,51 @@ export const Explorer = ({ match }: { match: any }) => {
     doSearch(query);
   }, [queryparam]);
 
-  const doSearch = (query: string) => {
-    if (query === '') return;
+  const getTransactionInformation = async (txid: string) => {
+    try {
+      let result = '';
+      let intputTotal = 0;
+      let outputTotal = 0;
+      const txCurrentDetails = await electrumx.getTransaction(txid);
 
-    setLoading(true);
-
-    let result = '';
-    electrumx.getTransaction(query).then((txDetails: any) => {
-      result = `${JSON.stringify(txDetails, null, 2)}`;
-
-      if (txDetails) {
+      if (txCurrentDetails) {
         // parse vins
         const vinAddresses: Address[] = [];
-        for (let i = 0; i < txDetails.vin.length; i++) {
-          const scriptHash = txDetails.vin[i].scriptSig.hex;
+        for (let i = 0; i < txCurrentDetails.vin.length; i++) {
+          const vin = txCurrentDetails.vin[i];
+
           // get addresses
+          const scriptHash = vin.scriptSig.hex;
           const address = electrumx.scripthashToAddress(scriptHash);
-          result += `\nvin[${i}]: ${address}`;
-          if (address) vinAddresses.push({ address, value: 0 });
+
+          // lookup input values
+          const txPreviousDetails = await electrumx.getTransaction(vin.txid);
+          // lookup the correct vout
+          const inputVin = txPreviousDetails.vout[vin.vout];
+          const inputValue = inputVin.value;
+          result += `\nvin[${i}]: ${address} => ${inputValue}`;
+
+          intputTotal += inputValue;
+          if (address) vinAddresses.push({ address, value: inputValue });
         }
         setSenders((prevState) => ({ ...prevState, addresses: vinAddresses }));
+        setTotalInputValue(intputTotal);
 
         // parse vouts
         const voutAddresses: Address[] = [];
         let decodedOpReturn = '';
-        for (let i = 0; i < txDetails.vout.length; i++) {
-          const vout = txDetails.vout[i];
+        for (let i = 0; i < txCurrentDetails.vout.length; i++) {
+          const vout = txCurrentDetails.vout[i];
           if (hasOpReturn([vout])) {
             decodedOpReturn = decodeOpReturnToAscii([vout]);
             result += `\nvout[${i}]: OP_RETURN ${decodedOpReturn}`;
           } else {
             const address = vout.scriptPubKey.addresses[0];
-            const { value } = vout;
-            result += `\nvout[${i}]: ${address} => ${value}`;
-            if (address) voutAddresses.push({ address, value });
+            const outputValue = vout.value;
+            result += `\nvout[${i}]: ${address} => ${outputValue}`;
+
+            outputTotal += outputValue;
+            if (address) voutAddresses.push({ address, value: outputValue });
           }
         }
         setRecipients((prevState) => ({
@@ -99,17 +112,31 @@ export const Explorer = ({ match }: { match: any }) => {
           addresses: voutAddresses,
           opReturn: { decoded: decodedOpReturn }
         }));
-      }
+        setTotalOutputValue(outputTotal);
 
-      try {
-        const parsedSlpData = slp.Utils.decodeTxData(txDetails);
-        result += `\n${JSON.stringify(parsedSlpData, null, 2)}`;
-      } catch (error) {
-        // An error will be thrown if the txid is not SLP.
+        try {
+          const parsedSlpData = slp.Utils.decodeTxData(txCurrentDetails);
+          result += `\n\n${JSON.stringify(parsedSlpData, null, 2)}`;
+        } catch (error) {
+          // An error will be thrown if the txid is not SLP.
+        }
+
+        result += `\n\n${JSON.stringify(txCurrentDetails, null, 2)}`;
       }
-      setResult(result);
-      setLoading(false);
-    });
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const doSearch = async (query: string) => {
+    if (query === '') return;
+    setLoading(true);
+
+    const result = await getTransactionInformation(query);
+
+    if (result) setResult(result);
+    setLoading(false);
   };
 
   const handleOnSearch = () => {
@@ -162,24 +189,48 @@ export const Explorer = ({ match }: { match: any }) => {
       </Space>
       {!isLoading && (
         <>
+          <Card title="Information">
+            <Text>Input Total: </Text>
+            <Text strong type="success">
+              {totalInputValue}
+            </Text>{' '}
+            <Text>Output Total: </Text>
+            <Text strong type="success">
+              {totalOutputValue}
+            </Text>{' '}
+            <Text>Transaction fee: </Text>
+            <Text strong type="success">
+              {(totalInputValue - totalOutputValue).toFixed(8)}
+            </Text>
+          </Card>
           <Row gutter={16} style={{ marginTop: '8px' }}>
             <Col span={12}>
               <Card title="Senders" bordered={true}>
                 {senders.addresses &&
                   senders.addresses.map((address: Address) => (
                     <div key={address.address}>
-                      <Text strong>{address.address}</Text>
+                      <Text strong type="success">
+                        {address.value}
+                      </Text>{' '}
+                      <Text>{address.address}</Text>
                     </div>
                   ))}
               </Card>
             </Col>
             <Col span={12}>
               <Card title="Recipients" bordered={true}>
-                {recipients.opReturn && <p>OP_RETURN: {recipients.opReturn.decoded}</p>}
+                {recipients.opReturn && (
+                  <>
+                    <Text>OP_RETURN:</Text>{' '}
+                    <Text strong type="success">
+                      {recipients.opReturn.decoded}
+                    </Text>
+                  </>
+                )}
                 {recipients.addresses &&
                   recipients.addresses.map((address: Address) => (
                     <div key={address.address}>
-                      <Text strong>{address.address}</Text>{' '}
+                      <Text>{address.address}</Text>{' '}
                       <Text strong type="success">
                         {address.value}
                       </Text>
